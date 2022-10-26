410ce26bfeef803651e0cd94a3b596dc08b7f025
Pedro Augusto dos Santos <83887299+pdrusr@users.noreply.github.com>
2022-10-26 11:47:34 -0300

    Add files via upload

--- src/Rotas.js
+++ src/Rotas.js
@@ -1,14 +1,45 @@
 import express from "express"
+import { Types, isValidObjectId } from "mongoose"
 import { postagem } from "./database/Modelos.js"
 
 const Rotas = express.Router()
 
 Rotas.get("/", async function(requisicao, resposta) {
-    resposta.render("inicio.pug")
+
+    await postagem.find({})
+
+        .then(function(resultados) {
+            console.log(resultados)
+            resposta.render("inicio.pug", { "resultados": resultados })
+        })
+
+        .catch(function(erro) {
+            console.log(erro)
+        })
+
 })
 
 Rotas.get("/postagem/:codigo", async function(requisicao, resposta) {
-    resposta.render("postagem.pug")
+
+    const { codigo } = requisicao.params
+
+    if (isValidObjectId(codigo)) {
+
+        await postagem.findById( Types.ObjectId(codigo) )
+
+            .then(function(resultado) {
+                console.log(resultado)
+                resposta.render("postagem.pug", { "resultado": resultado })
+            })
+            
+            .catch(function(erro) {
+                console.log(erro)
+            })
+    }
+
+    else {
+        resposta.send("<h1> Codigo postagem invalido! </h1>")
+    }
 })
 
 Rotas.get("/postar", async function(requisicao, resposta) {
@@ -16,11 +47,8 @@ Rotas.get("/postar", async function(requisicao, resposta) {
 })
 
 Rotas.post("/postar", async function(requisicao, resposta) {
-
     const corpo = requisicao.body
-
     console.log(corpo)
-
     const novapostagem = new postagem({
         descricao: corpo.descricao,
         conteudo: corpo.conteudo,
@@ -28,11 +56,8 @@ Rotas.post("/postar", async function(requisicao, resposta) {
         data_post: corpo.data_post,
         ativo: corpo.ativo == "on" ? true : false 
     })
-
     const resultado = await novapostagem.save()
-
     console.log(resultado)
-
     resposta.redirect("/")
 })
 
